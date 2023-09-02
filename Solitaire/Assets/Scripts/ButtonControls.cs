10ea28c7ff4252e1a264518eb9f0ffa8c81f341b
IslamG <i.ghretlli@masarat.ly>
2023-09-02 22:09:05 +0200

    Added support for toggling game options. UI and logic. Fixed game dropdown menu onValueChanged.

--- Solitaire/Assets/Scripts/ButtonControls.cs
+++ Solitaire/Assets/Scripts/ButtonControls.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -18,7 +19,11 @@ public class ButtonControls : MonoBehaviour
     public OnCardBackChanged backChanged = new();
 
     int lastSavedBack;
-    int selectedBack = 52;
+    DrawType lastDrawType;
+    ScoringType lastScoringType;
+    bool lastIsTimed;
+    bool lastHasStatusBar;
+    bool lastHasScoring;
 
     [DllImport("user32.dll")]
     private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
@@ -30,11 +35,20 @@ public class ButtonControls : MonoBehaviour
     [SerializeField]
     GameObject CardOptions;
     [SerializeField]
+    GameObject StatusBar;
+    [SerializeField]
+    GameObject timer;
+    [SerializeField]
+    GameObject score;
+    [SerializeField]
     TMP_Dropdown GameDropDown;
     [SerializeField]
     TMP_Dropdown HelpDropDown;
     [SerializeField]
     Sprite[] Sprites;
+    [SerializeField]
+    Toggle[] toggles;
+
     void Start()
     {
         currentScreenHeight = Screen.height;
@@ -45,9 +59,16 @@ public class ButtonControls : MonoBehaviour
         HelpDropDown.captionText.text = "Help";
 
         lastSavedBack = BoardSpriteManagement.CurrentCardSprite;
+        lastDrawType = OptionsManager.DrawCount;
+        lastScoringType= OptionsManager.Scoring;
+        lastIsTimed = OptionsManager.TimedGame;
+        lastHasStatusBar = OptionsManager.HasStatusBar;
+        lastHasScoring = OptionsManager.Scoring != ScoringType.None;
 
         EventManager.CardBackChangedInvoker(this);
     }
+
+    #region WindowHeader
     public void CloseButton() 
     {
         #if UNITY_EDITOR
@@ -56,7 +77,6 @@ public class ButtonControls : MonoBehaviour
                 Application.Quit();
         #endif
     }
-
     public void MaximizeButton()
     {
         isMaximized = !isMaximized;
@@ -92,18 +112,28 @@ public class ButtonControls : MonoBehaviour
         Screen.SetResolution(toWidth, toHeight, false);
 #endif
     }
-
     public void MinimizeButton()
     {
         ShowWindow(GetActiveWindow(), 2);
     }
+    #endregion
 
+    #region DropDown
     public void ShowGameOptionsWindow()
     {
+        toggles[0].SetIsOnWithoutNotify(OptionsManager.DrawCount is DrawType.Single);
+        toggles[1].SetIsOnWithoutNotify(OptionsManager.DrawCount is DrawType.Three);
+        toggles[2].SetIsOnWithoutNotify(OptionsManager.Scoring is ScoringType.Standard);
+        toggles[3].SetIsOnWithoutNotify(OptionsManager.Scoring is ScoringType.Vegas);
+        toggles[4].SetIsOnWithoutNotify(OptionsManager.Scoring is ScoringType.None);
+        toggles[5].SetIsOnWithoutNotify(OptionsManager.TimedGame);
+        toggles[6].SetIsOnWithoutNotify(OptionsManager.HasStatusBar);
+        toggles[7].SetIsOnWithoutNotify(false);
+        toggles[8].SetIsOnWithoutNotify(OptionsManager.Scoring != ScoringType.None);
+
         GameOptions.SetActive(true);
         CardOptions.SetActive(false);
     }
-
     public void ShowCardOptionsWindow()
     {
         CardOptions.SetActive(true);
@@ -113,26 +143,24 @@ public class ButtonControls : MonoBehaviour
     {
         GameOptions.SetActive(false);
     }
-
     public void HideCardOptionsWindow()
     {
         CardOptions.SetActive(false);
     }
-
     public void GameDropDownSelect()
     {
 
-        Debug.Log(GameDropDown.value);  
+        Debug.Log("before "+GameDropDown.value);  
 
         switch (GameDropDown.value)
         {
-            case 0: Camera.main.GetComponent<Board>().NewGame(); break;
-            case 1: ShowCardOptionsWindow(); break;
-            case 2: ShowGameOptionsWindow(); break;
-            case 3: CloseButton(); break;
+            case 0: GameDropDown.SetValueWithoutNotify(5); Camera.main.GetComponent<Board>().NewGame();  break;
+            case 1: GameDropDown.SetValueWithoutNotify(5); ShowCardOptionsWindow(); break;
+            case 2: GameDropDown.SetValueWithoutNotify(5); ShowGameOptionsWindow(); break;
+            case 3: GameDropDown.SetValueWithoutNotify(5); CloseButton();  break;
         }
 
-        GameDropDown.SetValueWithoutNotify(-1);
+        Debug.Log("after "+GameDropDown.value);
         GameDropDown.captionText.text = "Game";
     }
     public void HelpDropDownSelect()
@@ -143,7 +171,9 @@ public class ButtonControls : MonoBehaviour
             case 1: HelpDropDown.captionText.text = "Help"; break;
         }
     }
+    #endregion
 
+    #region CardOptions
     public void AcceptCardBack()
     {
         lastSavedBack = BoardSpriteManagement.CurrentCardSprite;
@@ -158,12 +188,74 @@ public class ButtonControls : MonoBehaviour
     }
     public void BackSelected(int val)
     {
-        //originalBack = 
-        selectedBack = val;
-
         val.ChangeCardBack();
         backChanged.Invoke();
     }
+    #endregion
+
+    #region GameOptions
+    public void SelectDrawMode(int val)
+    {
+        var drawMode = val == 1 ? DrawType.Single : DrawType.Three;
+        drawMode.SetDrawType();
+    }
+    public void SelectScoringMode(int val)
+    {
+        var scoringMode = val switch
+        {
+            0 => ScoringType.None,
+            1 => ScoringType.Standard,
+            2 => ScoringType.Vegas
+        };
+        scoringMode.SetScoringType();
+        StatusBar.SetActive(OptionsManager.HasStatusBar);
+    }
+    public void GameToggles(int val)
+    {
+        switch (val)
+        {
+            case 0: /*isTimed = !isTimed;*/ timer.SetActive(!lastIsTimed); (!lastIsTimed).SetTimedGame(); break;
+            case 1: /*hasStatusBar = !hasStatusBar;*/ StatusBar.SetActive(!lastHasStatusBar); (!lastHasStatusBar).SetHasStatusBar(); break;
+            case 2: break;
+            case 3: /*hasScoring = !hasScoring;*/ score.SetActive(!lastHasScoring); 
+                if (!lastHasScoring) 
+                    ScoringType.None.SetScoringType(); 
+                else
+                    lastScoringType.SetScoringType(); 
+                break;
+        };
+    }
+    public void AcceptGameOptions()
+    {
+        lastIsTimed = OptionsManager.TimedGame;
+        timer.SetActive(lastIsTimed);
+
+        lastHasScoring = OptionsManager.Scoring != ScoringType.None;
+        score.SetActive(lastHasScoring);
+
+        lastHasStatusBar = OptionsManager.HasStatusBar;
+        StatusBar.SetActive(lastHasStatusBar);
+
+        lastDrawType = OptionsManager.DrawCount;
+        lastScoringType = OptionsManager.Scoring;
+
+        HideGameOptionsWindow();
+    }
+    public void CancelGameOptions()
+    {
+        lastIsTimed.SetTimedGame();
+        timer.SetActive(lastIsTimed);
+        score.SetActive(lastHasScoring);
+        lastHasStatusBar.SetHasStatusBar();
+        StatusBar.SetActive(lastHasStatusBar);
+
+        lastDrawType.SetDrawType();
+        lastScoringType.SetScoringType();
+
+        HideGameOptionsWindow();
+    }
+    #endregion
+
     public void AddListener(UnityAction handler)
     {
         backChanged.AddListener(handler);
--- Solitaire/Assets/Scripts/OptionsManager.cs
+++ Solitaire/Assets/Scripts/OptionsManager.cs
@@ -3,14 +3,15 @@ public static class OptionsManager
     public static DrawType DrawCount { get; private set; } = DrawType.Single;
     public static ScoringType Scoring { get; private set; } = ScoringType.Standard;
     public static bool TimedGame { get; private set; } = true;
-    public static bool HasStatusBar => Scoring != ScoringType.None;
+    public static bool HasStatusBar { get; private set; } = true;
+    //public static bool HasScoring { get; private set; } = false;
     public static int CardBackIndex { get; private set; } = 52;
     public static bool LimitDiscardResets { get; private set; } 
 
     public static void SetDrawType (this DrawType type) => DrawCount = type;
     public static void SetScoringType(this ScoringType type) => Scoring = type;
     public static void SetTimedGame(this bool isTimed) => TimedGame = isTimed;
-   // public static void SetHasStatusBar(this bool hasBar) => HasStatusBar = hasBar;
+    public static void SetHasStatusBar(this bool hasStatusBar) => HasStatusBar = hasStatusBar;
     public static void SetBackIndext(this int index) => CardBackIndex = index;
     public static void SetCardBack(this int index) => index.ChangeCardBack();
     public static int NumberOfAllowedResets() => Scoring is ScoringType.Vegas ? 3 : 3;
